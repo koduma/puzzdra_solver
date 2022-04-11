418e5ffc15a0fc09ee4783c6036537dc23d94689
koduma <47982907+koduma@users.noreply.github.com>
2022-04-12 08:22:10 +0900

    Update Excalibur.cpp

--- Excalibur.cpp
+++ Excalibur.cpp
@@ -78,7 +78,7 @@ using namespace std;
 #define DROP 8//ドロップの種類//MAX9
 #define TRN 150//手数//MAX155
 #define BEAM_WIDTH 2800000//ビーム幅//MAX200000
-#define BEAM_WIDTH2 
+#define BEAM_WIDTH2 3
 #define PROBLEM 1//問題数
 #define BONUS 10//評価値改善係数
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
