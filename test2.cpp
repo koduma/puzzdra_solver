73ecebc827f436bfbbb59d5ca2bfa911d63cb06a
koduma <47982907+koduma@users.noreply.github.com>
2022-07-05 21:04:28 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -404,7 +404,7 @@ Action BEAM_SEARCH(F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev_dir,int n
 			}
 			if (i < MAX_TRN - 1) {
 			int pos=(temp.nowR*COL)+temp.nowC;
-			if(visited[pos][temp.hash]==0||visited[pos][temp.hash]>i+maxi){
+			if(visited[pos][temp.hash]==0||visited[pos][temp.hash]>=i+maxi){
 				visited[pos][temp.hash]=i+maxi;
 				dque.push_back(temp);
 				push_node++;
