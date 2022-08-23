5948be4db842299dbe80692a856c26d51bde22a3
koduma <47982907+koduma@users.noreply.github.com>
2022-08-23 15:12:36 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -556,6 +556,13 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 	}
 	cand.calc_path();
 	cand.calc_hash();
+	if(depth==DEPTH){
+	int output;
+	if(tmp.path[3]==','){output=(int)tmp.path.length()-4;}
+	else if(tmp.path[2]==','){output=(int)tmp.path.length()-3;}
+	cout<<"tesuu="<<output<<endl;
+	cout<<tmp.path<<endl;
+	}
 	if(depth==1){
 	cand.path_length+=i+1;
 	}
