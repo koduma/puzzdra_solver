437c1176e1d664e5a53ce68e13713284957c1363
tekitouk <47982907+koduma@users.noreply.github.com>
2022-09-17 20:25:58 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -584,12 +584,14 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 	}
 	cand.calc_path();
 	cand.calc_hash();
+	if(depth==DEPTH){
 	cout<<"hukasa="<<depth<<endl;
 	int output;
 	if(tmp.path[3]==','){output=(int)tmp.path.length()-4;}
 	else if(tmp.path[2]==','){output=(int)tmp.path.length()-3;}
 	cout<<"tesuu="<<output<<endl;
 	cout<<tmp.path<<endl;
+	}
 	if(depth==1){
 	cand.path_length+=i+1;
 	}
@@ -626,6 +628,8 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 		ll movei[(TRN/21)+1];
 		memcpy(bestAction.moving, ff[depth-1][j].movei, sizeof(movei));
 		bestAction.path=ff[depth-1][j].true_path;
+		cout<<"nest="<<depth<<endl;
+		cout<<"length="<<((int)bestAction.path.length()-3)<<endl;
 		return bestAction;
 	}
 	vec[ff[depth-1][j].path_length].push_front(j);
