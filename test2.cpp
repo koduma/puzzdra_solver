15963f19c766b4b2d6c80b16f9dbca94e6231233
tekitouk <47982907+koduma@users.noreply.github.com>
2022-11-06 20:02:35 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -87,7 +87,7 @@ using namespace std;
 #define TRN 150//手数//MAX155
 #define BEAM_WIDTH 2800000//MAX2800000
 #define BEAM_WIDTH2 3//MAX30
-#define PROBLEM 1//問題数
+#define PROBLEM 10//問題数
 #define BONUS 10//評価値改善係数
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define NODE_SIZE MAX(500,4*BEAM_WIDTH)
