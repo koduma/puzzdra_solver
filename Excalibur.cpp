5828300e0cc7b50b84119a2c1b617c555edc8b40
tekitouk <47982907+koduma@users.noreply.github.com>
2022-11-06 19:16:49 +0900

    Update Excalibur.cpp

--- Excalibur.cpp
+++ Excalibur.cpp
@@ -393,7 +393,7 @@ Action BEAM_SEARCH(F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev_dir,int n
 			}
 			if(fff[j].score>fff[j].prev_score){fff[j].improving=fff[j].improving+1;}
 			fff[j].prev_score=fff[j].score;
-			vec[fff[j].score+(BONUS*fff[j].improving)+(fff[j].nowR*3)+2000].push_front(j);
+			vec[fff[j].score+(BONUS*fff[j].improving)+(fff[j].nowR*3)+1500].push_front(j);
 			ks2++;
 			}
 		}
