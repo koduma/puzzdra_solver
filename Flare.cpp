a066839aa5dd1f1b4390d61253cb15c10173b89e
tekitouk <47982907+koduma@users.noreply.github.com>
2022-11-11 15:39:30 +0900

    Update Flare.cpp

--- Flare.cpp
+++ Flare.cpp
@@ -601,7 +601,7 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 	else{cand.path_length=(int)tmpp.path.length()-3;}
 	*/
 	printf("Beam=%d,visited=%d\n",cand.path_length,cand.calc_pl(cand.hash));
-	cand.path_length=min(cand.path_length,cand.calc_pl(cand.hash));
+	//cand.path_length=min(cand.path_length,cand.calc_pl(cand.hash));
 	if(stop!=tmpp.score){cand.path_length=TRN;}
 	pus[cand.path_length].push_front(cand);
 	cout<<"pos="<<cand.pos+1<<"/"<<ROW*COL<<endl;
