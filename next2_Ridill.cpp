3b38de9dd236e961ed965074a8d371e8f9a06d65
tekitouk <47982907+koduma@users.noreply.github.com>
2023-01-26 14:46:54 +0900

    Update next2_Ridill.cpp

--- next2_Ridill.cpp
+++ next2_Ridill.cpp
@@ -486,7 +486,7 @@ Action BEAM_SEARCH(F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev_dir,int n
 		start = omp_get_wtime();
 		dque.clear();
 		//deque<int>vec[5001];
-    priority_queue <pair<int,int> > vec;
+		priority_queue <pair<int,int> > vec;
 		int ks2 = 0;
 		bool congrats=false;
 		for (int j = 0; j < 4 * ks; j++) {
@@ -531,7 +531,7 @@ Action BEAM_SEARCH(F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev_dir,int n
 			if(fff[j].score>fff[j].prev_score){fff[j].improving=fff[j].improving+1;}
 			fff[j].prev_score=fff[j].score;
 			//vec[fff[j].score+(BONUS*fff[j].improving)+(fff[j].nowR*3)+2000].push_front(j);
-      int sco=fff[j].score+(BONUS*fff[j].improving)+(fff[j].nowR*3);
+			int sco=fff[j].score+(BONUS*fff[j].improving)+(fff[j].nowR*3);
 			if ((int)vec.size() >= BEAM_WIDTH && vec.top().first <= -sco) {continue;}
 			vec.push(make_pair(-sco,j));
 			if ((int)vec.size() > BEAM_WIDTH){ vec.pop();}    
