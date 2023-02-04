717752e570c705985df9fbc04f7dba26f7863fb9
tekitouk <47982907+koduma@users.noreply.github.com>
2023-02-04 14:02:52 +0900

    Update Flare.cpp

--- Flare.cpp
+++ Flare.cpp
@@ -646,8 +646,8 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 	cand.calc_hash();
 	if(tmp.path[3]==','){cand.path_length=(int)tmp.path.length()-4;}
 	else if(tmp.path[2]==','){cand.path_length=(int)tmp.path.length()-3;}
+	if(tmp.score!=stop){cand.path_length=TRN;}	
 	cand.path_length=min(cand.path_length,cand.calc_pl(cand.hash^zoblish_field2[cand.pos])+i+1);
-	if(tmp.score!=stop){cand.path_length=TRN;}
 	ff[depth-1][(4 * k) + j] = cand;
 	}//if(cand.prev
 	else {
