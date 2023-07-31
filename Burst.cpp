b898df563e31247451d878b59d2cc13fb2e69af5
tekitouk <47982907+koduma@users.noreply.github.com>
2023-07-31 20:55:24 +0900

    Update Burst.cpp

--- Burst.cpp
+++ Burst.cpp
@@ -1,25 +1,33 @@
 /*
 Windows10,Windows11,Linux,MacOS
+
 Linux導入手続き
 //メモリ容量確認
 free -h
+
 //g++インストール
 sudo apt install -y g++
+
 //wgetインストール
 sudo apt-get update
 sudo apt-get install -y wget
+
 //Burst.cppをダウンロード
 wget --no-check-certificate https://raw.githubusercontent.com/koduma/puzzdra_solver/master/Burst.cpp
+
 //hash_map.hpp,loguru.cpp,loguru.hppをダウンロード
 wget --no-check-certificate https://raw.githubusercontent.com/koduma/puzzdra_solver/master/hash_map.hpp
 wget --no-check-certificate https://raw.githubusercontent.com/koduma/puzzdra_solver/master/loguru.cpp
 wget --no-check-certificate https://raw.githubusercontent.com/koduma/puzzdra_solver/master/loguru.hpp
+
 //ビーム幅調整
 vi Burst.cpp
+
 //コンパイル
 Linux:g++ -O2 -std=c++11 -fopenmp -mbmi2 -lpthread Burst.cpp loguru.cpp -o Burst -mcmodel=large -ldl
 Windows10,Windows11:g++ -O2 -std=c++11 -fopenmp -mbmi2 -lpthread Burst.cpp loguru.cpp -o Burst -mcmodel=large
 MacOS:g++ -std=c++11 -fopenmp -mbmi2 -lpthread Burst.cpp loguru.cpp -o Burst -ldl
+
 //run
 ./Burst
 //input
@@ -991,7 +999,7 @@ Action BEAM_SEARCH(int depth,F_T f_field[ROW][COL],int maxi,int MAX_TRN,int prev
 	hc.path=bestAction.path;
 	hc.ptom();	
 	hc.calc_hashchain();	
-	hc.depth=0;	
+	hc.depth=depth;	
 		
 	memcpy(g_field,root_field,sizeof(g_field));
 	int tgt=0;
