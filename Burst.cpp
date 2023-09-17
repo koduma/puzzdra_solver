5ab08928dc5ad64b1b8288c5025538071f6c0652
tekitouk <47982907+koduma@users.noreply.github.com>
2023-09-17 12:36:30 +0900

    Update Burst.cpp

--- Burst.cpp
+++ Burst.cpp
@@ -148,7 +148,7 @@ ll fallBB(ll p,ll rest,ll mask);
 multimap<ll, ll> visited;
 ll zoblish_field2[ROW*COL];
 int BW[DEPTH+1]={BEAM_WIDTH,BEAM_WIDTH2,10,1};
-int DELTA_P[DEPTH+1]={150,150,150,150};
+int DELTA_P[DEPTH+1]={40,40,40,40};
 emilib::HashMap<ll, bool> visited2[DEPTH];
 int counter=0;
 int read_file_mode;
